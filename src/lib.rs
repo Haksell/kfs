46cd9d0989c337ce9c25ecf61c734d878def38f0
Axel Brisse <axel.brisse@gmail.com>
2024-03-10 11:42:23 +0100

    vga_buffer.rs and better Makefile

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,15 @@
 use core::panic::PanicInfo;
 
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    // WARNING: we have a very small stack and no guard page
-
-    let hello = b"Hello World!";
-    let color_byte = 0x1f;
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i * 2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-
+    vga_buffer::clear_screen();
+    println!("KFS {}", 6 * 7);
     loop {}
 }
 
