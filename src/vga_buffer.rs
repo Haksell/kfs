8a7cd928fea2a7dc16a6dcb1964f3c452433618c
Axel Brisse <axel.brisse@gmail.com>
2024-03-10 11:47:42 +0100

    green

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -112,7 +112,7 @@ impl fmt::Write for Writer {
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        color_code: ColorCode::new(Color::LightGreen, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
