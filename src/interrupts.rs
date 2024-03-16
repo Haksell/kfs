01ce9373143cea5d58d42ea81172ff0d44f6a152
Axel Brisse <axel.brisse@gmail.com>
2024-03-16 18:34:16 +0100

    stop timer spam

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -42,7 +42,7 @@ pub fn init() {
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
+    // print!(".");
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
