52fecc8a6c84af389d073ae08b550f2bf516b295
Axel Brisse <axel.brisse@gmail.com>
2024-03-24 04:33:24 +0100

    clean ooga

--- src/shell.rs
+++ src/shell.rs
@@ -128,10 +128,7 @@ impl Shell {
                 KeyCode::F2 => self.switch_screen(1),
                 KeyCode::F3 => self.switch_screen(2),
                 KeyCode::F4 => self.switch_screen(3),
-                key => {
-                    use crate::println;
-                    println!("{:#?}", key) // TODO: remove
-                }
+                _ => {}
             },
         }
     }
