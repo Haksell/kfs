211eef1c470551c28f7fb598bd736d1eb11db30d
Axel Brisse <axel.brisse@gmail.com>
2024-05-05 11:18:54 +0200

    gdt + idt kind of good?

--- asm/boot.asm
+++ asm/boot.asm
@@ -5,14 +5,51 @@ section .text
 bits 32
 
 start:
-    cli
     mov esp, stack_top
     call check_multiboot
     call check_cpuid
-    call kernel_main
+    lgdt [gdt.pointer]
+    mov ax, gdt.kernel_stack
+    mov ss, ax
+    mov ax, gdt.kernel_data
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    jmp gdt.kernel_code:kernel_main
 
 section .bss
 align 4096
 stack_bottom:
     resb 4096 * 128
 stack_top:
+
+section .rodata
+align 8
+
+%macro DEFINE_GDT_SEGMENT 1
+    dw 0xFFFF     ; limit low
+    dw 0x0000     ; base low
+    db 0x00       ; base middle
+    db %1         ; access
+    db 0b11001111 ; granularity
+    db 0x00       ; base high
+%endmacro
+
+gdt:
+    dq 0
+.kernel_code: equ $ - gdt
+    DEFINE_GDT_SEGMENT 0b10011011
+.kernel_data: equ $ - gdt
+    DEFINE_GDT_SEGMENT 0b10010011
+.kernel_stack: equ $ - gdt
+    DEFINE_GDT_SEGMENT 0b10010111
+.user_code: equ $ - gdt
+    DEFINE_GDT_SEGMENT 0b11111011
+.user_data: equ $ - gdt
+    DEFINE_GDT_SEGMENT 0b11110011
+.user_stack: equ $ - gdt
+    DEFINE_GDT_SEGMENT 0b11110111
+.pointer:
+    dw $ - gdt - 1
+    dd gdt
