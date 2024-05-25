b687007bf837295e7906d8529115d78219a4d13b
Pierre LE LEZ <ple-lez@student.42.fr>
2024-05-25 13:45:51 +0200

    frame allocator working

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,27 @@ mod port;
 mod shell;
 mod vga_buffer;
 
+extern crate multiboot2;
+
+use crate::memory::frame::FrameAllocator;
 use core::arch::asm;
 use core::panic::PanicInfo;
 
+use multiboot2::{BootInformationHeader, ElfSectionFlags};
+
 #[no_mangle]
 pub extern "C" fn kernel_main(multiboot_header_address: usize) {
-    let boot_info = unsafe { memory::multiboot::load(multiboot_header_address) };
-    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
-    let elf_sections_tag = boot_info
-        .elf_sections_tag()
-        .expect("Elf-sections tag required");
+    let boot_info = unsafe {
+        multiboot2::BootInformation::load(multiboot_header_address as *const BootInformationHeader)
+            .unwrap()
+    };
+
+    let our_boot_info = unsafe { memory::multiboot::load(multiboot_header_address) };
+
+    let memory_map_tag = our_boot_info
+        .memory_map_tag()
+        .expect("Memory map tag required");
+    let elf_sections_tag = boot_info.elf_sections().expect("Elf-sections tag required");
 
     vga_buffer::WRITER.lock().clear_vga_buffer();
     shell::SHELL.lock().init();
@@ -31,13 +42,58 @@ pub extern "C" fn kernel_main(multiboot_header_address: usize) {
     }
 
     println!("kernel sections:");
-    for section in elf_sections_tag.sections() {
-        println!(
-            "    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
-            section.start_address(),
-            section.size(),
-            section.flags()
-        );
+    for section in elf_sections_tag {
+        if section.flags() != ElfSectionFlags::empty() {
+            println!(
+                "    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+                section.start_address(),
+                section.size(),
+                section.flags()
+            );
+        }
+    }
+
+    let kernel_start = boot_info
+        .elf_sections()
+        .unwrap()
+        .map(|s| s.start_address())
+        .min()
+        .unwrap();
+
+    let kernel_end = boot_info
+        .elf_sections()
+        .unwrap()
+        .map(|s| s.start_address() + s.size())
+        .max()
+        .unwrap();
+
+    let multiboot_end = multiboot_header_address + boot_info.total_size();
+
+    println!(
+        "kernel_start: {:#x}, kernel_end: {:#x}",
+        kernel_start, kernel_end
+    );
+    println!(
+        "multiboot_start: {:#x}, multiboot_end: {:#x}",
+        multiboot_header_address, multiboot_end
+    );
+
+    let mut frame_allocator = memory::frame::AreaFrameAllocator::new(
+        kernel_start as usize,
+        kernel_end as usize,
+        multiboot_header_address,
+        multiboot_end,
+        memory_map_tag.memory_areas(),
+    );
+
+    for i in 0..10 {
+        match frame_allocator.allocate_frame() {
+            None => {
+                println!("allocated {} frames", i);
+                break;
+            }
+            Some(frame) => println!("{:?}", frame),
+        }
     }
 
     interrupts::init();
