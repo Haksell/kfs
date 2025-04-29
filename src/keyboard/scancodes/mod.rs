a84400ee9b4af6da77406d93686071f6c3d221b8
Haksell <axel.brisse@gmail.com>
2025-04-29 20:44:10 +0200

    empty kfs2 branch

--- src/keyboard/scancodes/mod.rs
+++
@@ -1,9 +0,0 @@
-mod set1;
-
-pub use self::set1::ScancodeSet1;
-
-use super::{Error, KeyEvent};
-
-pub trait ScancodeSet {
-    fn add_byte(&mut self, code: u8) -> Result<Option<KeyEvent>, Error>;
-}
--- src/memory/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod frame;
-pub mod multiboot;
-pub mod paging;
-
-pub const PAGE_SIZE: usize = 4096;
